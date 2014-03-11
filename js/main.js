7b99ffaf448a44ba4493d59f7e1e76c02122b707
Kyryl Kozlov <kyryl_kozlov@MSHFD03.minto.local>
2014-03-11 09:22:12 -0400

    still playing with main.js

--- js/main.js
+++ js/main.js
@@ -1,11 +1,19 @@
 jQuery(document).ready(function($){
-	alert("why not working");
 	var slide=false;
 	var descrSelected=0;
-	alert("there we go");
 	$( '#list-of-games' ).on('click', '.game-line', function(e) {
-		alert("here");
-		
+		if(descrSelected!=0){
+			$( "#game-"+descrSelected ).slideUp( "fast", function() {
+				//$( "#game-"+descrSelected ).css('display', 'none');
+			});	
+		}
+		alert("now here");
+		$( "#game-"+this.id ).slideDown( "fast", function() {
+			//$( "#game-"+this.id ).css('display', 'block');
+		});
+		alert("and now here");
+		descrSelected = this.id;
+		alert(descrSelected);
 	});
 	
 	//hide story actions
